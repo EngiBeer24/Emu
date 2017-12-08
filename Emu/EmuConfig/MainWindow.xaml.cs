fb5d5e532ef73825f214bed25b11db980b3d3f7b
computergeek24 <computergeek24@gmail.com>
2017-12-08 01:12:40 -0800

    Progress

---
+++ Emu/EmuConfig/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace EmuConfig
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SystemsConfig systemsConfig = new SystemsConfig();
+        public MainWindow()
+        {
+            InitializeComponent();
+            systemsConfig.LoadSystemsConfigFile();
+            RomDirTextBox.Text = systemsConfig.GetRomRoot();
+        }
+
+        private void RomDirButtonClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                RomDirTextBox.Text = dialog.SelectedPath;
+            }
+        }
+    }
+}
---
+++ Emu/EmuConfig/SystemsConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace EmuConfig
+{
+    public class SystemsConfig
+    {
+        //private String workingDir = Directory.GetCurrentDirectory();
+        private String workingDir = @"C:\Projects\Emulation\ProjectEmu";
+        private String systemsConfigSubPath = @"\EmulationStation\.emulationstation\es_systems.cfg";
+        private String romRoot = null;
+
+        private string GetCOnfigFilePath()
+        {
+            return workingDir + systemsConfigSubPath;
+        }
+
+        public string GetRomRoot()
+        {
+            return romRoot;
+        }
+
+        public void LoadSystemsConfigFile()
+        {
+            XDocument configFile = XDocument.Load(GetCOnfigFilePath());
+            foreach (var node in configFile.Descendants("system"))
+            {
+                String systemName = (String)node.Element("name");
+                String systemPath = (String)node.Element("path");
+                String systemRoot = systemPath.Replace($"\\{systemName}", "");
+                if (romRoot != null && romRoot != systemRoot)
+                {
+                    // TODO Throw if there are romRoot path differences between systems
+                }
+                else
+                {
+                    romRoot = systemRoot;
+                }
+            }
+        }
+    }
+}
